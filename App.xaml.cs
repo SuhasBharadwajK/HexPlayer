7aa9016368d1827549d2f668a41f25227145b825
Suhas Bharadwaj <suhas.five@gmail.com>
2015-06-17 21:09:03 -0700

    First Commit
    First commit of hex player. Committing now because I didn't commit
    before.

---
+++ App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HexPlayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public string color, name;        
+        public void onStartup(object sender, StartupEventArgs args)
+        {
+            if (args.Args.Length != 0)
+            {
+                name = args.Args[1];
+                if (args.Args[0] == "blue")
+                {
+                    color = "#0066FF";
+                }
+                else
+                {
+                    color = "#FF2A2A";
+                }
+            }            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HexPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Brush brush;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var appObject = App.Current as App;            
+            this.Title = "Player " + appObject.name;
+            brush = (Brush)new BrushConverter().ConvertFromString(appObject.color);
+            
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse.Fill = brush;
+        }
+    }
+}
